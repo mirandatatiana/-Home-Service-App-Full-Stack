5a33f0f517473245785d5124c86792fe87f7d0dd
tati <miranda.tatianayamila@gmail.com>
2024-05-06 17:43:14 +0200

    config

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,86 @@
-import { Descope } from '@descope/nextjs-sdk';
-// you can choose flow to run from the following without `flowId` instead
-// import { SignInFlow, SignUpFlow, SignUpOrInFlow } from '@descope/nextjs-sdk'
+import NextAuth from "next-auth/next";
+import { NextAuthOptions } from "next-auth"
 
-const Page = () => {
-	return (
-		<Descope
-			flowId="sign-up-or-in"
-			onSuccess={(e) => console.log('Logged in!')}
-			onError={(e) => console.log('Could not logged in!')}
-			redirectAfterSuccess="/"
-			// redirectAfterError="/error-page"
-		/>
-	);
-};
+
+export const authOptions  = {
+    providers: [
+    {
+        id: "descope",
+        name: "Descope",
+        type: "oauth",
+        wellKnown: `https://api.descope.com/P2dwkyWomBkHvDnfNz7te0NftYFU/.well-known/openid-configuration`,
+        authorization: { params: { scope: "openid email profile" } },
+        idToken: true,
+        clientId: "P2dwkyWomBkHvDnfNz7te0NftYFU",
+        clientSecret: "<Descope Access Key>",
+        checks: ["pkce", "state"],
+        profile(profile) {
+            return {
+                id: profile.sub,
+                name: profile.name,
+                email: profile.email,
+                image: profile.picture,
+            }
+        },
+    }],
+    callbacks: {
+        async jwt({token, account, profile}) {
+            if (account) {
+                return {
+                    ...token,
+                    access_token: account.access_token,
+                    expires_at: Math.floor(Date.now() / 1000 + account.expires_in),
+                    refresh_token: account.refresh_token,
+                    profile: {
+                      name: profile?.name,
+                      email: profile?.email,
+                      image: profile?.picture,
+                  },
+                }
+            } else if (Date.now() < token.expires_at * 1000) {
+                return token
+            } else {
+                try {
+                    const response = await fetch("https://api.descope.com/oauth2/v1/token", {
+                        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+                        body: new URLSearchParams({
+                            client_id: "P2dwkyWomBkHvDnfNz7te0NftYFU",
+                            client_secret: "<Descope Access Key>",
+                            grant_type: "refresh_token",
+                            refresh_token: token.refresh_token,
+                        }),
+                        method: "POST",
+                    })
+    
+                    const tokens = await response.json()
+    
+                    if (!response.ok) throw tokens
+    
+                    return {
+                        ...token,
+                        access_token: tokens.access_token,
+                        expires_at: Math.floor(Date.now() / 1000 + tokens.expires_in),
+                        refresh_token: tokens.refresh_token ?? token.refresh_token,
+                    }
+                } catch (error) {
+                    console.error("Error refreshing access token", error)
+                    return {...token, error: "RefreshAccessTokenError"}
+                }
+            }
+        },
+    
+        async session({session, token}) {
+            if (token.profile) {
+              session.user = token.profile;
+            }
+    
+            session.error = token.error
+            session.accessToken = token.access_token
+            return session
+        },
+      }
+}  
+
+
+export default NextAuth(authOptions)
+
